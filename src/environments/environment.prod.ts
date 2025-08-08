32ea1f0b085f5843988f5986085669f05ed51ee8
Peter Atito <peterclermy232@gmail.com>
2025-08-08 09:44:59 +0300

    update on the enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://sacco-banking-latest.onrender.com/api',
+  apiUrl: 'https://sacco-banking-latest.onrender.com/api/v1',
   appName: 'SACCO Banking',
   version: '1.0.0',
   apiTimeout: 30000,
