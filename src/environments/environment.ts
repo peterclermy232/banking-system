95e627fb9f00a2901558e7935cbf46be88785406
Peter Atito <peterclermy232@gmail.com>
2025-10-02 09:38:43 +0300

    update on the endpoint

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8002/api/v1',
+  apiUrl: 'https://banking-system-backend-5bvu.onrender.com/api/v1',
   appName: 'SACCO Banking Dev',
   version: '1.0.0-dev',
   apiTimeout: 10000,
